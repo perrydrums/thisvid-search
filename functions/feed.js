5af4c6ed9bb8874719992a428ee80d46c532a687
Perry Janssen <perry.janssen@iodigital.com>
2024-02-03 23:18:32 +0000

    feat: Add feed function

--- functions/feed.js
+++ functions/feed.js
@@ -1,14 +1,6 @@
-const chromium = require('@sparticuz/chromium');
-const puppeteer = require('puppeteer-extra');
-const AdBlockerPlugin = require('puppeteer-extra-plugin-adblocker');
-const adBlocker = AdBlockerPlugin({
-  blockTrackers: true,
-});
-puppeteer.use(adBlocker);
-
 const headers = {
   'Content-Type': 'application/json',
-  'Cache-Control': 'public, max-age=31536000',
+  'Cache-Control': 'public, max-age=80000',
   'Netlify-Vary': 'query',
 };
 
@@ -26,37 +18,11 @@ exports.handler = async function (event, context) {
     };
   }
 
-  const options = {
-    executablePath: process.env.CHROME_EXECUTABLE_PATH || (await chromium.executablePath()),
-  };
+  const result = await fetch(
+    `https://main-bvxea6i-stzzsy25tku52.de-2.platformsh.site/feed?username=${username}&password=${password}`,
+  );
+  const videos = await result.json();
 
-  if (!process.env.CHROME_EXECUTABLE_PATH) {
-    options.args = chromium.args;
-  }
-
-  const browser = await puppeteer.launch(options);
-  const page = await browser.newPage();
-  await page.goto('https://thisvid.com/login.php');
-
-  await page.type('#login_username', username);
-  await page.type('#login_pass', password);
-  await page.click('button.login');
-
-  await page.waitForNavigation();
-
-  await page.goto('https://thisvid.com/my_friends_events/');
-
-  const videos = await page.evaluate(() => {
-    const elements = Array.from(document.querySelectorAll('.entry'));
-    return elements.map((element) => {
-      const author = element.querySelector('.user-avatar .tumbpu').title;
-      const title = element.querySelector('.thumbs-items .tumbpu').title;
-      const url = element.querySelector('.thumbs-items .tumbpu').href;
-      return { author, title, url };
-    });
-  });
-
-  await browser.close();
   const currentDate = new Date().toLocaleDateString('en-GB').replaceAll('/', '-');
 
   return {
